6ede46ddf305c6edcc612638655f2755e1fe97be
Lucas F Mancan <lucasfmancan@hotmail.com>
2024-02-13 14:23:49 -0300

    criado query busca extrato

--- src/main/java/app/ConsultarExtratoUsecaseImpl.java
+++ src/main/java/app/ConsultarExtratoUsecaseImpl.java
@@ -3,18 +3,32 @@ package app;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+
+import javax.sql.DataSource;
 
 @Singleton
 public class ConsultarExtratoUsecaseImpl implements ConsultarExtratoUsecase {
     private final DSLContext context;
 
     @Inject
-    public ConsultarExtratoUsecaseImpl(DSLContext context) {
-        this.context = context;
+    public ConsultarExtratoUsecaseImpl(DataSource dataSource) {
+        this.context = DSL.using(dataSource, SQLDialect.POSTGRES);
     }
+
+    //
+
+    select saldo, limite, t.valor, t.tipo, t.descricao, t.realizada_em from clientes c
+            join transacoes t on t.cliente_d = c.cliente_id
+            where c.cliente_id = $1
+
+    //
 
     @Override
     public Extrato gerarExtratoPorIdCliente(int idCliente) {
+
+
         return null;
     }
 }
